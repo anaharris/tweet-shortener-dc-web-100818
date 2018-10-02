32945225681d1cac025d35c117b0e3892063ef27
Ana Harris <vana1990@gmail.com>
2018-10-02 02:26:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,12 @@
 def dictionary 
   words = {
-    hi => "hello",
-    2 => ["to", "two", "too"],
-    4 => ["for", "four"],
-    b => "be",
-    u => "you",
-    @ => "at",
-    & => "and"
+    "hi" => "hello",
+    "2" => ["to", "two", "too"],
+    "4" => ["for", "four"],
+    "b" => "be",
+    "u" => "you",
+    "@" => "at",
+    "&" => "and"
   }
 end
 
