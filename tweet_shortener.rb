f6488847df3b4d313d965d018bd3352690867714
Ana Harris <vana1990@gmail.com>
2018-10-02 02:25:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,17 +10,21 @@ def dictionary
   }
 end
 
+
 def word_substituter 
   
 end 
+
 
 def bulk_tweet_shortener
   
 end 
 
+
 def selective_tweet_shortener 
   
 end
+
 
 def shortened_tweed_truncator 
   
