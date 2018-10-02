09173c10a2e007dba7d07dcc5a88b307b10f0665
Ana Harris <vana1990@gmail.com>
2018-10-02 02:55:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,8 +20,8 @@ def word_substituter (tweet)
   tweet_arr.each do |word|
     if tweet_arr.include?(word) && keys_arr.include?(word) == true 
     tweet_arr[tweet_arr.index(word)] = dictionary[word]
-    else 
-      word
+  end
+  
 end 
 
 
